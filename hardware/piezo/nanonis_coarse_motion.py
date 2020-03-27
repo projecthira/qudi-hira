9b2256ef0c0770e0986b58d2c29b29035b0033b3
Dinesh Pinto <d.pinto@fkf.mpg.de>
2020-03-27 14:50:50 +0100

    Added full support for piezo coarse positioning with LabView interface

--- hardware/piezo/nanonis_coarse_motion.py
+++ hardware/piezo/nanonis_coarse_motion.py
@@ -24,27 +24,49 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 from core.module import Base
-import os
 from core.configoption import ConfigOption
 import win32com.client
+
+
+def _get_lv_axis(axis):
+    if axis == "X" or axis == "x":
+        return 1
+    elif axis == "Y" or axis == "y":
+        return 2
+    elif axis == "Z" or axis == "z":
+        return 3
+    else:
+        # All axes are referenced
+        return 0
+
+
+def _get_lv_direction(direction):
+    if direction == "x+" or direction == "X+":
+        return 0
+    elif direction == "x-" or direction == "X-":
+        return 1
+    elif direction == "y+" or direction == "Y+":
+        return 2
+    elif direction == "y-" or direction == "Y-":
+        return 3
+    elif direction == "z+" or direction == "Z+":
+        return 4
+    elif direction == "z-" or direction == "Z-":
+        return 5
+    else:
+        raise ValueError("Invalid direction {} specified.".format(direction))
 
 
 class NanonisCoarseMotion(Base):
     """Provides software backend to the Nanonis via LabView.
     """
-    _vi_base_folder_path = ConfigOption('vi_base_folder_path', missing='error')
-    _vi_file_motor_frequency_amplitude_get = ConfigOption('vi_file_motor_frequency_amplitude_get',  missing='error')
-    _vi_file_motor_frequency_amplitude_set = ConfigOption('vi_file_motor_frequency_amplitude_set',  missing='error')
-    _vi_file_motor_start_move = ConfigOption('vi_file_motor_start_move',  missing='error')
-    _vi_file_motor_step_counter_get = ConfigOption('vi_file_motor_step_counter_get',  missing='error')
-    _vi_file_motor_stop_move = ConfigOption('vi_file_motor_stop_move',  missing='error')
-
-    # Create paths to all VI files
-    _vi_path_motor_frequency_amplitude_get = os.path.join(_vi_base_folder_path, _vi_file_motor_frequency_amplitude_get)
-    _vi_path_motor_frequency_amplitude_set = os.path.join(_vi_base_folder_path, _vi_file_motor_frequency_amplitude_set)
-    _vi_path_motor_start_move = os.path.join(_vi_base_folder_path, _vi_file_motor_start_move)
-    _vi_path_motor_step_counter_get = os.path.join(_vi_base_folder_path, _vi_file_motor_step_counter_get)
-    _vi_path_motor_stop_move = os.path.join(_vi_base_folder_path, _vi_file_motor_stop_move)
+    _vi_path_motor_frequency_amplitude_get = ConfigOption('vi_path_motor_frequency_amplitude_get',  missing='error')
+    _vi_path_motor_frequency_amplitude_set = ConfigOption('vi_path_motor_frequency_amplitude_set',  missing='error')
+    _vi_path_motor_start_move = ConfigOption('vi_path_motor_start_move',  missing='error')
+    _vi_path_motor_step_counter_get = ConfigOption('vi_path_motor_step_counter_get',  missing='error')
+    _vi_path_motor_stop_move = ConfigOption('vi_path_motor_stop_move',  missing='error')
+    _sample_group = ConfigOption('sample_group', default=0, missing='warn')
+    _tip_group = ConfigOption('tip_group', default=1, missing='warn')
 
     #_host = ConfigOption('host', default='localhost', missing='error')
 
@@ -55,46 +77,165 @@ class NanonisCoarseMotion(Base):
     def on_activate(self):
         try:
             # Dispatches a signal to open 64-bit LabView
-            self.labview = win32com.client.Dispatch("Labview.Application")
+            # dynamic dispatch is required to ensure late-binding of application, otherwise the code will not run
+            self.labview = win32com.client.dynamic.Dispatch("Labview.Application")
             # Load all the required VIs for piezo coarse motion
+            self.labview._FlagAsMethod("GetVIReference")
             self.motor_freq_amp_get = self.labview.GetVIReference(self._vi_path_motor_frequency_amplitude_get)
             self.motor_freq_amp_set = self.labview.GetVIReference(self._vi_path_motor_frequency_amplitude_set)
             self.motor_start_move = self.labview.GetVIReference(self._vi_path_motor_start_move)
             self.motor_step_counter_get = self.labview.GetVIReference(self._vi_path_motor_step_counter_get)
             self.motor_stop_move = self.labview.GetVIReference(self._vi_path_motor_stop_move)
+            self.open_front_panels()
+            self.log.info("Finished loading all required LabView VIs.")
             return 0
         except Exception as exc:
             self.log.error("Error during LabView connection : {}".format(exc))
             return 1
 
     def on_deactivate(self):
-        self.labview.Quit()
+        try:
+            self.log.info("LabView Save Dialog should be dealt with.")
+            self.labview.Quit()
+        except TypeError:
+            pass
         return 0
 
-    def _get_amplitude(self, axis=None):
-        if axis is not None:
-            self.motor_freq_amp_get.setControlValue("Axis (Default)", axis)
-        self.motor_freq_amp_get.Call()
+    def open_front_panels(self):
+        try:
+            self.motor_freq_amp_get.OpenFrontPanel()
+        except TypeError:
+            pass
+        try:
+            self.motor_freq_amp_set.OpenFrontPanel()
+        except TypeError:
+            pass
+        try:
+            self.motor_start_move.OpenFrontPanel()
+        except TypeError:
+            pass
+        try:
+            self.motor_step_counter_get.OpenFrontPanel()
+        except TypeError:
+            pass
+        try:
+            self.motor_stop_move.OpenFrontPanel()
+        except TypeError:
+            pass
+
+    def get_amplitude(self, axis=None):
+        """ Get driving amplitude of piezo motion from LabView.
+
+        :param axis: string either "x", "y" or "z". If None then the Axis (Default) is called.
+        :return: float of piezo driving amplitude
+        """
+        lv_axis = _get_lv_axis(axis)
+        self.motor_freq_amp_get.setControlValue("Axis (Default)", lv_axis)
+        try:
+            self.motor_freq_amp_get.Run()
+        except TypeError:
+            pass
         amplitude = self.motor_freq_amp_get.getControlValue("Amplitude (V)")
         return amplitude
 
-    def _get_frequency(self, axis=None):
-        if axis is not None:
-            self.motor_freq_amp_get.setControlValue("Axis (Default)", axis)
-        self.motor_freq_amp_get.Call()
+    def get_frequency(self, axis=None):
+        """ Get driving frequency of piezo motion from LabView.
+
+        :param axis: string either "x", "y" or "z". If None then the Axis (Default) is called.
+        :return: float of piezo driving frequency
+        """
+        lv_axis = _get_lv_axis(axis)
+        self.motor_freq_amp_get.setControlValue("Axis (Default)", lv_axis)
+        try:
+            self.motor_freq_amp_get.Run()
+        except TypeError:
+            pass
         frequency = self.motor_freq_amp_get.getControlValue("Frequency (Hz)")
         return frequency
 
-    def _set_amplitude(self, amplitude, axis=None):
-        if axis is not None:
-            self.motor_freq_amp_get.setControlValue("Axis (Default)", axis)
-        self.motor_freq_amp_get.setControlValue("Amplitude (V)", amplitude)
-        self.motor_freq_amp_get.Call()
+    def set_amplitude_and_frequency(self, amplitude, frequency, axis=None):
+        """Set driving amplitude and frequency of piezo motion from LabView.
+        These are done together to as the LabView interface has no memory.
+
+        :param frequency: float Piezo driving frequency in Hertz (Hz)
+        :param amplitude: float Piezo driving amplitude in Volts (V)
+        :param axis: string either "x", "y" or "z". If None then the Axis (All) is called
+        :return: 0 if successful
+        """
+        lv_axis = _get_lv_axis(axis)
+        self.motor_freq_amp_set.setControlValue("Axis (All)", lv_axis)
+        self.motor_freq_amp_set.setControlValue("Amplitude (V) (NaN=no change)", str(amplitude))
+        self.motor_freq_amp_set.setControlValue("Frequency (Hz) (NaN=no change)", str(frequency))
+
+        try:
+            self.motor_freq_amp_set.Run()
+        except TypeError:
+            pass
         return 0
 
-    def _set_frequency(self, frequency, axis=None):
-        if axis is not None:
-            self.motor_freq_amp_get.setControlValue("Axis (Default)", axis)
-        self.motor_freq_amp_get.setControlValue("Frequency (Hz)", frequency)
-        self.motor_freq_amp_get.Call()
+    def move_rel(self, group, direction, steps):
+        """Move piezos a specific number of steps in a given direction.
+
+        :param group: Piezo group as assigned in Nanonis
+        :param direction: Direction of movement "X+", "X-" etc.
+        :param steps: Number of steps to take in that direction
+        :return:
+        """
+        if group == "tip":
+            self.motor_start_move.setControlValue("Group", self._tip_group)
+        elif group == "sample":
+            self.motor_start_move.setControlValue("Group", self._sample_group)
+        else:
+            self.log.warn("Group parameter {} is not 'tip' or 'sample'.".format(group))
+            self.motor_start_move.setControlValue("Group", group)
+
+        lv_direction = _get_lv_direction(direction)
+        self.motor_start_move.setControlValue("Direction", lv_direction)
+        self.motor_start_move.setControlValue("Number of steps", steps)
+        try:
+            self.motor_start_move.Run()
+        except TypeError:
+            pass
+        return 0
+
+    def get_step_counters(self):
+        """Get current step count.
+
+        :return: dict of {x, y, z} step counts
+        """
+        x_steps = self.motor_step_counter_get.getControlValue("Step Counter X")
+        y_steps = self.motor_step_counter_get.getControlValue("Step Counter Y")
+        z_steps = self.motor_step_counter_get.getControlValue("Step Counter Z")
+        return {"x_steps": x_steps, "y_steps": y_steps, "z_steps": z_steps}
+
+    def reset_step_counters(self):
+        """Reset all step counters, if not already reset.
+
+        :return: 0 if successful
+        """
+        if not self.motor_step_counter_get.getControlValue("Reset X (F)"):
+            self.motor_step_counter_get.setControlValue("Reset X (F)", True)
+        if not self.motor_step_counter_get.getControlValue("Reset Y (F)"):
+            self.motor_step_counter_get.setControlValue("Reset Y (F)", True)
+        if not self.motor_step_counter_get.getControlValue("Reset Z (F)"):
+            self.motor_step_counter_get.setControlValue("Reset Z (F)", True)
+        try:
+            self.motor_step_counter_get.Run()
+        except TypeError:
+            pass
+
+        self.motor_step_counter_get.setControlValue("Reset Z (F)", False)
+        self.motor_step_counter_get.setControlValue("Reset Z (F)", False)
+        self.motor_step_counter_get.setControlValue("Reset Z (F)", False)
+        return 0
+
+    def stop(self):
+        """Stop all motion of piezos.
+
+        :return: 0 if successful
+        """
+        try:
+            self.motor_stop_move.Run()
+        except TypeError:
+            pass
         return 0
