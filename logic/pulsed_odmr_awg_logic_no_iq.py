71c21d60920674822ce8708f383ea30980133760
Dinesh Pinto <d.pinto@fkf.mpg.de>
2021-12-17 00:36:26 +0100

    Fixed laser external state when opening ODMR, updated headers

--- logic/pulsed_odmr_awg_logic_no_iq.py
+++ logic/pulsed_odmr_awg_logic_no_iq.py
@@ -118,7 +118,6 @@ class AwgPulsedODMRLogic(GenericLogic):
         self._laser_logic = self.laserlogic()
 
         # self._awg_device.reset()
-        self._laser_logic.set_external_state(True)
 
         # Get hardware constraints
         mw_limits = self.get_hw_constraints()
